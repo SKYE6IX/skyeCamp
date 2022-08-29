837ad2eff08cccd29f1078cf0650a2e67b6da5ec
SKYE <skye@192.168.1.2>
2022-08-29 16:39:20 +0300

    updating homepage

--- app.js
+++ app.js
@@ -18,9 +18,9 @@ const User = require('./models/user');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require("helmet");
 const MongoStore = require('connect-mongo');
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+const dbUrl =  process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
-
+// process.env.DB_URL || 
 
 //Requireing campgrounds routes
 const campgroundRoutes = require('./routes/campgrounds');
@@ -42,13 +42,6 @@ mongoose.connect(dbUrl)
     console.log('Oh No Error!!')
     console.log(err);
 })
-
-// const db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'connection error'));
-// db.once('open', ()=>{
-//     console.log('Database Connected');
-// })
-
 
 
 // MIDDLEWARE OR APP SETTINGS
--- cloudinary/index.js
+++ cloudinary/index.js
@@ -3,7 +3,6 @@ const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 //configure cloudinary
-
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_KEY,
